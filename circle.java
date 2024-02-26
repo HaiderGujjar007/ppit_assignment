efb29eea2cd5b4f7ff5ad6cd43e2551613bb95f6
HaiderGujjar <haiderrasheed168@gmail.com>
2024-02-26 10:44:47 +0500

    First commit

---
+++ circle.java
@@ -0,0 +1,21 @@
+class circle{
+    double radius;
+    double calculatearea(){
+        return Math.PI* radius* radius;
+    }
+double calculatePerimeter(){
+    return 2 *Math.PI *radius;
+}
+
+}
+public class circle {
+    public static void main(string[] args) {
+        Circle c1=new Circle();
+        C1.radius=5.0;
+        System.out.println("Area of the circle: " + c1.calculateArea());
+        System.out.println("Perimeter of the circle: " + c1.calculatePerimeter());
+
+    }
+}
+}
+public class 
